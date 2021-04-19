aabe61c7228b41301e97fdc9efc6b002238ffbf4
boramcom@daum.net <boramcom@daum.net>
2021-04-19 11:32:45 +0900

    2교시 커밋

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+//const PORT2 = 5001;//포트를 서비스별로 분리가 가능
+//예, 댓글: 6000 reply.js, 게시판:5500 board.js, 인증서비스: 5002 login.js
+//위 방식이 마이크로서비스(MSA)방식 입니다.
 
 var app = express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  //.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
   // 푸시기능, 챠트 실시간 렌더링 하는 소켓앱 시작
   //http서버 객체를 생성시 app(express) 프레임워크를 전달
@@ -15,4 +18,6 @@ var app = express()
   //socket.io 소켓 통신 객체생성 시 http를 객체로 전달
   var io = require('socket.io').listen(http);
   //.listen은 클라이언트에서 서버로 접속을 받기위해 대기하는 명령
-  http.listen(PORT, function(){});
+  http.listen(PORT, function(){
+    console.log('앱이 시작 되었습니다. 포트번호: ' + PORT);
+  });
